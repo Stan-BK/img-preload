9b3e241d70db801477ac1e553aee893288b875b5
Kkk <2568143457@qq.com>
2022-11-03 11:14:04 +0800

    chore: change image pool's structure

--- src/pool.ts
+++ src/pool.ts
@@ -1,7 +1,17 @@
-export const images: HTMLImageElement[] = []
+export interface ImagePoolItem{
+  image: HTMLImageElement
+  comment: Comment
+}
+
+export const images: ImagePoolItem[] = []
 export const loadedImages: HTMLImageElement[] = []
 export const failedImages: HTMLImageElement[] = []
 
 export function initPool(imgs: ArrayLike<HTMLImageElement>) {
-  images.splice(0, images.length, ...Array.from(imgs))
+  images.splice(0, images.length, ...Array.from(imgs).map(img => {
+    return {
+      image: img,
+      comment: document.createComment('img-place')
+    }
+  }))
 }
