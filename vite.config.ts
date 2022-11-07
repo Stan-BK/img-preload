f9490b32cd9f0aa89db283d4cb46740665ae767e
Kkk <2568143457@qq.com>
2022-11-07 18:22:16 +0800

    chore: change build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  build: {
+    lib: {
+      entry: './src/index.ts',
+      name: 'ImgPreload',
+      fileName: 'index',
+      formats: ['es', 'cjs']
+    }
+  },
   server: {
     open: '/example/index.html'
   }
