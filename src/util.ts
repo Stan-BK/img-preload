03dec9986c7d846fc858725fe69940ea9093e3e7
Kkk <2568143457@qq.com>
2022-11-08 11:59:47 +0800

    fix: fix error in access attribute offsetTop/offsetLeft

--- src/util.ts
+++ src/util.ts
@@ -1,12 +1,12 @@
 export function getOffset(elem: HTMLElement) {
-  let parent = elem.parentElement
+  let parent = elem.offsetParent as HTMLElement
   let offsetLeft = elem.offsetLeft
   let offsetTop = elem.offsetTop
 
   while(parent) {
     offsetLeft += parent.offsetLeft
     offsetTop += parent.offsetTop
-    parent = parent.parentElement
+    parent = parent.offsetParent as HTMLElement
   }
 
   return {
